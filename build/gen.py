2ce54ea4cb2eacd6a93283ee8dd0de1fbdc0ca16
devkirill <deviatkin-kirill@ya.ru>
2015-04-07 19:34:17 +0500

    последняя задача с парой недоделок

--- build/gen.py
+++ build/gen.py
@@ -1,10 +1,23 @@
 import sys
+import re
 
-for param in sys.argv:
-	print param
+reload(sys)
+sys.setdefaultencoding('ascii')
 
-f = open(sys.argv[1], 'r')
+f = open(sys.argv[1], 'r').read()
 
-a = f.read()
-re.search(r'^(OK|DR|ZA) \d+ \d+ \d+ \d+$', text)
+replace = []
 
+def repl(matchobj):
+	replace.append([matchobj.group(1), matchobj.group(2)])
+	return ''
+
+def include(matchobj):
+	return open(matchobj.group(1), 'r').read()
+
+f = re.sub(r'#include \"([^\"]+)\".*(?=\n)', include, f)
+f = re.sub(r'#replace \"(.+)\", \"(.*)\".*\n', repl, f)
+for pair in replace:
+	# print pair
+	f = re.sub(pair[0], pair[1], f)
+print f
